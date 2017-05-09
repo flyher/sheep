f93c2782c91a3e4f2a3340ef4b2fb490f93e3614
flyher <flyher@users.noreply.github.com>
2017-05-09 21:56:03 +0800

    empty master

--- server/routes/tab.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-exports.TopList = function (req, res) {
-    fs.readFile('public/mock/tab/top.json', function (err, data) {
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
-}
