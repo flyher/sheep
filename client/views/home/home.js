bad39a2b47365aa4e342bb20995e377dc0adb80f
flyher <flyher@users.noreply.github.com>
2017-05-17 16:32:20 +0800

    add event for entries

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -34,16 +34,25 @@ var Home = Page.extend({
     'info': {
       '.projectname click': 'projcenameClick',
       '.describe click': 'describeClick'
+    },
+    'entries': {
+      '.opensource-link click': 'opensourcelinkClick'
     }
   },
   render: function () {
-    var self = this, headerData = new Object(), titleData = null, menuData = null, infoData = null;
+    var self = this, headerData = new Object(), titleData = null, menuData = null, infoData = null, pageModel = new Object();
     // mock data
     // 你也可以设置data为null，并启动sever，数据会从server ajax方式拿
+
     headerData.title = { "success": true, "data": { "logo": "logo.png", "projectname": "sheep", "url": "https://github.com/flyher/sheep", "describe": "A front-end rendering solution, support IE6, IE7, IE8 and modern browser" } };
     headerData.menu = { "success": true, "data": { "list": [{ "id": 1, "key": "home", "value": "Home", "url": "#" }, { "id": 2, "key": "about", "value": "About", "url": "https://github.com/flyher/sheep/blob/master/README.md" }, { "id": 3, "key": "github", "value": "Github", "url": "https://github.com/flyher/sheep" }, { "id": 4, "key": "blog", "value": "Blog", "url": "http://blog.99diary.com/2017/03/06/sheep" }] } };
     infoData = { "success": true, "data": { "projectname": "sheep", "url": "https://github.com/flyher/sheep", "describe": "A front-end rendering solution, support IE6, IE7, IE8 and modern browser" } };
-    entriesData = { "success": true, "data": { "list": [{ "id": 1, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 2, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 3, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 4, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 5, "title": "Subheading", "descri6be": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 6, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }] } };
+    entriesData = { "success": true, "data": { "list": [{ "id": 1, "title": "client", "describe": "JsRender and Handlebars.", "url": "#" }, { "id": 2, "title": "server", "describe": "NodeJs, use express mock data api.", "url": "#" }, { "id": 3, "title": "JsRender", "describe": "JsRender is a light-weight but powerful templating engine, highly extensible, and optimized for high-performance rendering, without DOM dependency. It is designed for use in the browser or on Node.js, with or without jQuery.", "url": "https://github.com/BorisMoore/jsrender" }, { "id": 4, "title": "Handlebars", "describe": "Handlebars provides the power necessary to let you build semantic templates effectively with no frustration. Handlebars is largely compatible with Mustache templates. In most cases it is possible to swap out Mustache with Handlebars and continue using your current templates.", "url": "https://github.com/wycats/handlebars.js" }, { "id": 5, "title": "es5-shim", "describe": "ECMAScript 5 compatibility shims for legacy (and modern) JavaScript engines.", "url": "https://github.com/es-shims/es5-shim" }, { "id": 6, "title": "webpack", "describe": "webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.", "url": "https://github.com/webpack/webpack" }] } };
+    // bind data to pageModel temp ,v0.3 test
+    pageModel.headerData = headerData;
+    pageModel.infoData = infoData;
+    pageModel.entriesData = entriesData;
+
     // 
     header(headerData, function (headerHtml) {
       $('.header_').html(headerHtml);//多次回调获取到渲染后的header的html
@@ -61,6 +70,7 @@ var Home = Page.extend({
     });
     // 2.也可以全部渲染完成注册事件
     self.regEvents('menu', self);
+    self.regEvents('entries', self);
   },
   regEvents: function (tplName, params) {
     /// <summary>注册指定模板事件</summary>
@@ -103,6 +113,14 @@ var Home = Page.extend({
       // 点击描述信息
       describeClick: function () {
         alert('You click the descibe');
+      }
+    },
+    entries: {
+      opensourcelinkClick: function () {
+        var url = $(this).attr('attr-url');
+        if (url !== '#') {
+          window.open(url, '_blank');
+        }
       }
     }
   }
