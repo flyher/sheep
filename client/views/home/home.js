47afd953c16c97fad8ed2da92c2305fe17dce111
flyher <flyher@users.noreply.github.com>
2017-05-17 14:47:20 +0800

    add event

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -30,7 +30,7 @@ var Home = Page.extend({
     'menu': {
       '.menu-link click': 'menulinkClick' //点击链接事件，这里可以用a标签直接渲染，这个方法为了演示绑定事件触发
     },
-    // 
+    // info
     'info': {
       '.projectname click': 'projcenameClick',
       '.describe click': 'describeClick'
@@ -38,9 +38,10 @@ var Home = Page.extend({
   },
   render: function () {
     var self = this, headerData = new Object(), titleData = null, menuData = null, infoData = null;
-    // demo data
+    // mock data
+    // 你也可以设置data为null，并启动sever，数据会从server ajax方式拿
     headerData.title = { "success": true, "data": { "logo": "logo.png", "projectname": "sheep", "url": "https://github.com/flyher/sheep", "describe": "A front-end rendering solution, support IE6, IE7, IE8 and modern browser" } };
-    headerData.menu = { "success": true, "data": { "list": [{ "id": 1, "key": "home", "value": "Home", "url": "#" }, { "id": 2, "key": "about", "value": "About", "url": "https://github.com/flyher/sheep/blob/master/README.md" }, { "id": 3, "key": "github", "value": "Github", "url": "https://github.com/flyher/sheep" }] } };
+    headerData.menu = { "success": true, "data": { "list": [{ "id": 1, "key": "home", "value": "Home", "url": "#" }, { "id": 2, "key": "about", "value": "About", "url": "https://github.com/flyher/sheep/blob/master/README.md" }, { "id": 3, "key": "github", "value": "Github", "url": "https://github.com/flyher/sheep" }, { "id": 4, "key": "blog", "value": "Blog", "url": "http://blog.99diary.com/2017/03/06/sheep" }] } };
     infoData = { "success": true, "data": { "projectname": "sheep", "url": "https://github.com/flyher/sheep", "describe": "A front-end rendering solution, support IE6, IE7, IE8 and modern browser" } };
     entriesData = { "success": true, "data": { "list": [{ "id": 1, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 2, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 3, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 4, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 5, "title": "Subheading", "descri6be": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }, { "id": 6, "title": "Subheading", "describe": "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum." }] } };
     // 
@@ -81,17 +82,27 @@ var Home = Page.extend({
     $(_DOMSelector).bind(_EventName, params, self[_DOMFunction]);
   },
   queryEvents: {
+    // menu link的事件
     menu: {
+      // 点击link
       menulinkClick: function () {
-        window.open();
+        var url = $(this).attr('attr-url');
+        if (url !== '#') {
+          window.open(url, '_blank');
+        } else {
+          alert('This page is home page!');
+        }
       }
     },
+    // info 中的事件
     info: {
+      // 点击项目名称
       projcenameClick: function () {
         alert('You click the projectname');
       },
-      describeClick: function (d) {
-        // console.log('describeClick-' + d);
+      // 点击描述信息
+      describeClick: function () {
+        alert('You click the descibe');
       }
     }
   }
